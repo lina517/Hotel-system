c7cfcf35504add96159d75a75ce37465ed039043
lina517 <119998586+lina517@users.noreply.github.com>
2024-05-21 23:20:11 +0100

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -2,27 +2,46 @@ import java.util.Scanner;
 
 public class Room {
     private int IDchambre;
-    private String Type;
+    private String Typeroom;
     private date dateDebut;
     private date dateFin;
 
     public Room(int IDchambre,String type ) {
         this.IDchambre = IDchambre;
-        this.Type = Type;        
+        this.Typeroom = Typeroom;        
     }
-
+    public Room(date datedDebut,date dateFin) {
+        this.dateDebut = datedDebut;
+        this.dateFin = dateFin;
+    }
 	public int getIDchambre() {
         return IDchambre;
     }
     public void release() {
     	this.dateDebut = null;
     	this.dateFin = null;
+    	
     }
+   public boolean isReserved() {
+		// dateDut verification de date debut et fin <> null
+		return false;
+	}
+    public void book(date dateDebut, date dateFin) {
+    	        this.dateDebut = dateDebut;
+    	        this.dateFin = dateFin;
+    	        // demande la datedebut et fin from user 
+    }
+    	 
 public static void main(String[] args) {
     
 }
 
-	public boolean isReserved() {
+	
+	public Object getStartdate() {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
+	}
+	public Object getdateDebut() {
+		// TODO Auto-generated method stub
+		return null;
 	}}
